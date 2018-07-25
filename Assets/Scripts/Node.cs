716cf2aa0a0af89e1105f42c2b82ee2028b4c600
chetobros <chetobros@gmail.com>
2018-07-25 10:45:50 -0500

    BFS script insertated

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node<T>{
+	List<Node<T>> children = new List<Node<T>>();
+	Node<T> dad = null;
+	T value;
+	bool visited = false;
+	public Node(T value){
+	}
+	public Node(T value,Node<T> dad){
+		this.value = value;
+		this.dad = dad;
+	}
+	public void Add(T value){
+		Node<T> child = new Node<T> (value, this);
+		children.Add (child);
+	}
+	public void AddToChildren(T value,T valueOfChild){
+		foreach (Node<T> node in children) {
+			if (node.value.Equals (valueOfChild)) {
+				node.Add (value);
+			}
+		}
+	}
+	public bool HaveChildren(){
+		if (this.children.Count == 0)
+			return false;
+		else
+			return true;
+	}
+	public Node<T> FindValue(T value){
+		foreach (Node<T> node in children) {
+			if (node.value.Equals(value)) {
+				return node;
+			}
+		}
+		return null;
+	}
+	public void PrintChildren(){
+		foreach (Node<T> node in children) {
+			Debug.Log (node.value);
+		}
+	}
+	public void PrintChildrenChildrens(T valueOfChild){
+		foreach(Node<T> node in children){
+			if (node.value.Equals (valueOfChild)) {
+				Debug.Log ("Node with value " + valueOfChild + " have...");
+				node.PrintChildren ();
+			}
+		}
+	}
+	public void DFS(Node<T> root){
+		if (root.HaveChildren ()) {
+			foreach (Node<T> node in children) {
+				if (!node.visited) {
+					node.visited = true;
+					Debug.Log ("visiting " + node.value);
+					node.DFS (node);
+				}
+			}
+		}
+	}
+	public void BFS(Node<T> root,int depth){
+		int level = 1;
+		if (root.HaveChildren ()) {
+			foreach (Node<T> node in children) {
+				if (depth == level) {
+					Debug.Log (node.value);
+				} else {
+					level++;
+					node.BFS (node,level);
+				}
+			}
+
+		}
+	}
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	Node<int> tree;
+	void Start () {
+		tree = new Node<int>(1);
+		tree.Add (2);
+		tree.Add (3);
+		tree.Add (4);
+		tree.AddToChildren (5, 2);
+		tree.AddToChildren (6, 2);
+		tree.AddToChildren (7, 3);
+		tree.AddToChildren (8, 4);
+		tree.AddToChildren (9, 4);
+		tree.DFS (tree);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
